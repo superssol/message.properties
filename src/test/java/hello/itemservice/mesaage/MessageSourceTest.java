6336c1f932ec3e0099527a5677dd09631c95dc36
superssol <hansole10@gmail.com>
2022-12-22 11:31:49 +0900

    Initial commit

---
+++ src/test/java/hello/itemservice/mesaage/MessageSourceTest.java
@@ -0,0 +1,45 @@
+package hello.itemservice.mesaage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+public class MessageSourceTest {
+
+    @Autowired
+    MessageSource ms;
+
+
+    @Test
+    void helloMessage() {
+        System.out.println(Locale.getDefault());
+        String result = ms.getMessage("hello", null, null);
+        assertThat(result).isEqualTo("안녕");
+    }
+
+    @Test
+    void notFoundMessageCode() {
+        assertThatThrownBy(() -> ms.getMessage("no_code", null, null))
+                .isInstanceOf(NoSuchMessageException.class);
+    }
+
+    @Test
+    void notFoundMessageCodeDefault() {
+        String result = ms.getMessage("no_code", null, "기본 메시지", null);
+        assertThat(result).isEqualTo("기본 메시지");
+    }
+
+    @Test
+    void argumentMessage() {
+        String result = ms.getMessage("hello.name", new Object[]{"spring"}, null);
+        assertThat(result).isEqualTo("안녕 spring");
+    }
+}
